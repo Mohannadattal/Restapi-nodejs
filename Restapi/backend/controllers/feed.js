a5a5102821eb20cde8d344d8387ce873efef9688
Mohannadgithub <mohannadattal85@gmail.com>
2023-02-28 14:26:43 +0100

    Fetching Lists of Posts

---
+++ Restapi/backend/controllers/feed.js
@@ -0,0 +1,32 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post',
+        imageUrl: 'images/apple-iphone-14-pro-max-1.jpg',
+        creator: {
+          name: 'Mohannad',
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Create pos in db
+
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+    },
+  });
+};
